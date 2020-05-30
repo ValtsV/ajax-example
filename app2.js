21203a9171691745fb26cb3c9146870298e6bc0c
Valts Vilcans <valts03@inbox.lv>
2020-05-30 12:24:51 +0200

    [same, but w/ array]

--- app2.js
+++ app2.js
@@ -20,4 +20,28 @@ const loadUser = () => {
 
   xhr.send();
 };
+
+const loadUsers = () => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "users.json", true);
+
+  xhr.onload = function () {
+    if (this.status == 200) {
+      const users = JSON.parse(this.responseText);
+      let output = "";
+
+      users.forEach((user) => {
+        output += `<ul>
+        <li>${user.id}</li>
+        <li>${user.name}</li>
+        <li>${user.email}</li>
+        </ul?`;
+      });
+      document.getElementById("users").innerHTML = output;
+    }
+  };
+  xhr.send();
+};
+
 document.getElementById("button").addEventListener("click", loadUser);
+document.getElementById("buttonOther").addEventListener("click", loadUsers);
